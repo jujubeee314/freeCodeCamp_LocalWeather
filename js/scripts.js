8e6dae6a37cff50fe7be4a87a722e928865dbaaf
Justin Mendoza <mendoj@gmail.com>
2016-02-14 22:43:24 -0500

    finished weather app

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 
-  var lon;
-  var lat;
+  var lon = -74.1046149;
+  var lat = 40.708054;
 
   if (navigator.geolocation) {
      navigator.geolocation.getCurrentPosition(function(position) {
-       lat = Math.round(position.coords.latitude);
+       console.log("inside navigator")
+       lat = position.coords.latitude;
        console.log(lat);
-       lon = Math.round(position.coords.longitude);
+       lon = position.coords.longitude;
        console.log(lon);
      });
    }
@@ -26,10 +27,42 @@ $(document).ready(function() {
     var country = json.sys.country;
     $(".location").html(city + ", " + country);
 
-    var temp = json.main.temp - 273.15;
-    $(".temp").html(temp);
+    var cel = Math.round(json.main.temp - 273.15);
+    var fah = Math.round(cel * 9/5 + 32);
+    $(".temp").html(cel + "&deg;");
+    $(".convert").on("click", function () {
+      if($(".convert").html() == "C") {
+        $(".temp").html(fah + "&deg;");
+        $(".convert").html("F");
+      }
+      else if ($(".convert").html() == "F") {
+        $(".temp").html(cel + "&deg;");
+        $(".convert").html("C");
+      }
+    });
 
-    var desc = json.weather.main;
+    var desc = json.weather[0].main;
+    console.log(desc);
+    switch (desc) {
+      case "Clear":
+        $(".weatherbg").css("background-image","url('https://snapshotsofwanaka.files.wordpress.com/2013/06/20130606-161727.jpg')");
+        break;
+      case "Snow":
+        $(".weatherbg").css("background-image","url('https://experiencepreferred.files.wordpress.com/2010/02/p2102152.jpg')");
+        break;
+      case "Rain":
+        $(".weatherbg").css("background-image","url('http://onpasture.com/wp-content/uploads/2015/02/rain-04.jpg')");
+        break;
+      case "Thunderstorm":
+        $(".weatherbg").css("background-image","url('http://www.clouds365.com/blog/wp-content/uploads/2013/06/4-01-13-lightning-wallcloud-paducah-tx.jpg')");
+        break;
+      case "Clouds":
+        $(".weatherbg").css("background-image","url('http://www.photos-public-domain.com/wp-content/uploads/2012/04/gray-overcast-sky.jpg')");
+        break;
+      default:
+
+
+    }
     $(".desc").html(desc);
 
   });
